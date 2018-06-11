8be64b7f11dd49a52fca0288e12d29145cb182fc
Jim Speth <jim@speth.com>
2018-06-11 18:10:41 -0400

    Added beginning of a command parser.

--- moo.py
+++ moo.py
@@ -1,26 +1,30 @@
-#!/usr/bin/env python -i
+#!/usr/bin/env python
 
+import click
+import cmd
+import code
 import json
+import sys
 import uuid
 
 ## World
 class World:
-    path = "world.json"
+    path = 'world.json'
 
     def __init__(self):
-        self.rooms = {"0": Room(id="0", description="This is the beginning of the world.")}
+        self.rooms = {'0': Room(id='0', description='This is the beginning of the world.')}
 
     def load(self):
-        data = open(self.path, "r").read()
+        data = open(self.path, 'r').read()
         world = json.loads(data)
         rooms = {}
-        for room_id, room_dict in world["rooms"].iteritems():
+        for room_id, room_dict in world['rooms'].iteritems():
             rooms[room_id] = Room(**room_dict)
         self.rooms = rooms
 
     def save(self):
         data = json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=2, separators=(',', ': '))
-        with open(self.path, "w") as f:
+        with open(self.path, 'w') as f:
             f.write(data)
 
 
@@ -34,19 +38,19 @@ class Room:
 
     def look(self):
         if self.name:
-            print "* " + self.name + " *"
-        print self.description or "You see nothing here."
+            print '* ' + self.name + ' *'
+        print self.description or 'You see nothing here.'
         if self.exits:
             directions = self.exits.keys()
             if len(directions) == 1:
-                # one direction: "You can go up."
-                print "You can go " + directions[0] + "."
+                # one direction: 'You can go up.'
+                print 'You can go ' + directions[0] + '.'
             elif len(directions) == 2:
-                # two directions: "You can go east or west."
-                print "You can go " + directions[0] + " or " + directions[1] + "."
+                # two directions: 'You can go east or west.'
+                print 'You can go ' + directions[0] + ' or ' + directions[1] + '.'
             else:
-                # lots of directions: "You can go up, east, or west."
-                print "You can go " + ", ".join(directions[:-1]) + ", or " + directions[-1] + "."
+                # lots of directions: 'You can go up, east, or west.'
+                print 'You can go ' + ', '.join(directions[:-1]) + ', or ' + directions[-1] + '.'
 
 
 ## Player
@@ -58,32 +62,39 @@ class Player:
         self.room = None
 
     def say(self, message):
-        print self.name + " says, \"" + message + "\""
+        print self.name + ' says, \'' + message + '\''
 
     def emote(self, message):
-        print self.name + " " + message
+        print self.name + ' ' + message
 
 
 ## Commands
 
+@click.command()
 def load():
     world.load()
 
+@click.command()
 def save():
     world.save()
 
+@click.command()
 def look():
     here.look()
 
+@click.command()
+@click.argument('name', required=False)
 def name(name=None):
     if name:
         here.name = name
     else:
         if here.name:
-            print "* " + here.name + " *"
+            print '* ' + here.name + ' *'
         else:
-            print "This room has no name."
+            print 'This room has no name.'
 
+@click.command()
+@click.argument('description', required=False)
 def describe(description=None):
     if description:
         here.description = description
@@ -91,9 +102,11 @@ def describe(description=None):
         if here.description:
             print here.description
         else:
-            print "This room has no description."
+            print 'This room has no description.'
 
-def go(direction):
+@click.command()
+@click.argument('direction', required=False)
+def go(direction=None):
     global here
     if direction in here.exits:
         here = world.rooms[here.exits[direction]]
@@ -101,22 +114,89 @@ def go(direction):
     else:
         print "You can't go that way."
 
-def dig(direction, back="back"):
+@click.command()
+@click.argument('direction', required=False)
+@click.argument('back', required=False, default='back')
+def dig(direction=None, back='back'):
     global here
-    new_room = Room(exits={back: here.id})
-    world.rooms[new_room.id] = new_room
-    here.exits[direction] = new_room.id
-    here = new_room
+    if direction is None:
+        print 'You must give a direction.'
+    elif direction in here.exits:
+        print 'That direction already exists.'
+    else:
+        new_room = Room(exits={back: here.id})
+        world.rooms[new_room.id] = new_room
+        here.exits[direction] = new_room.id
+        here = new_room
 
 
 ## Start MonkaMOO
 
-print "Welcome to MonkaMOO!"
-
 world = World()
 world.load()
-here = world.rooms["0"]
-here.look()
+here = world.rooms['0']
 
-charlotte = Player(name="Charlotte")
-jim = Player(name="Jim")
+#print 'Welcome to MonkaMOO!'
+#here.look()
+
+charlotte = Player(name='Charlotte')
+jim = Player(name='Jim')
+
+## Command Parsing
+
+@click.group()
+def cli():
+    pass
+
+@click.command()
+def interact():
+    # see https://docs.python.org/2/library/code.html?highlight=interpreter
+    code.interact(local=globals())
+
+@click.command()
+def help():
+    print 'help'
+
+@click.command()
+def quit():
+    sys.exit(0)
+
+cli.add_command(interact)
+cli.add_command(help)
+cli.add_command(quit)
+
+cli.add_command(load)
+cli.add_command(save)
+cli.add_command(look)
+cli.add_command(name)
+cli.add_command(describe)
+cli.add_command(go)
+cli.add_command(dig)
+
+
+## Shell
+
+class Shell(cmd.Cmd):
+    intro = 'Welcome to MonkaMOO!'
+    prompt = 'moo> '
+    file = None
+
+    def do_pass(self, arg):
+        pass
+
+    def precmd(self, line):
+        if line == 'EOF':
+            print
+            sys.exit(0)
+        args = line.split(' ')
+        cli.main(args, standalone_mode=False)
+        return 'pass'
+
+
+## Launch
+
+def main():
+    Shell().cmdloop()
+
+if __name__ == '__main__':
+    main()
