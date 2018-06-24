d6d15e3abe6601f2fc8e3436bf5064c49e5a3f4d
Jim Speth <jim@speth.com>
2018-06-24 19:34:52 -0400

    Removed test for room, it *should* always exist.

--- ball.py
+++ ball.py
@@ -4,18 +4,12 @@ class Ball(Thing):
     """ A simple ball. """
 
     def bounce(self, command):
-        if not self.room:
-            return
         self.room.announce(command.player, 'The ball bounces up and down.')
 
     def roll(self, command):
-        if not self.room:
-            return
         self.room.announce(command.player, 'The ball rolls away.')
 
     def throw(self, command):
-        if not self.room:
-            return
         self.room.announce(command.player, 'The ball flies in the air.')
         def fall():
             self.room.announce(command.player, 'The ball falls to the ground, bounces a few times, then settles.')
